87f125891bebfb156e106770c5942269afffb838
MuktarRashidat <ogunjinmirashidat@gmail.com>
2026-06-28 02:16:57 +0100

    added jinja2, bug fix

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-@app.route('/home')
+@app.route('/')
 def home():
-    return render_template('home.html')
+    logged = True
+    food = "pizza"
+    fruit = "mango"
+    return render_template('home.html', foods = food, fruits = fruit, loggedin = logged)
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    username = "aideibama"
+    booklist = ["atomic habit","deep work", "the alchemist"]
+    return render_template('about.html', username=username, book = booklist)
 if __name__ == '__main__':
     app.run(debug=True)
