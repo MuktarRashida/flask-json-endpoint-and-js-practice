181691d92d302765a732a3b0bde0c45311b537a2
MuktarRashidat <ogunjinmirashidat@gmail.com>
2026-06-30 03:40:26 +0100

    add js function for my about.html, fix bug, made improvements

--- static/script.js
+++ static/script.js
@@ -1,11 +1,22 @@
 let btn = document.getElementById('btn')
 let display = document.getElementById('box')
 
-btn.addEventListener('click', async function displays()
-{input = document.getElementById('txt').value
-const response = await fetch ('/api/user')
+if (btn){btn.addEventListener('click', async function displays()
+{const response = await fetch ('/api/user')
     const data = await response.json()
     console.log(typeof data)
     console.log(data)
     display.textContent = data.name
-})
+})}
+
+let btn2 = document.getElementById('btn2')
+let display2 = document.getElementById('box2')
+
+if (btn2){btn2.addEventListener('click', async function displays()
+{input2 = document.getElementById('txt2').value
+const response = await fetch ('/api/role?names=' + input2)
+    const data = await response.json()
+    console.log(typeof data)
+    console.log(data)
+    display2.textContent = data.result
+})}
